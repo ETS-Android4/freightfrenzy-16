24773b26ae92734334d4114adca3b36da41e14f0
Chris Simms <csimms@Chriss-MacBook-Pro.local>
2022-01-27 15:07:11 -0800

    added new (empty) automode file

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot2AutonomousV1.java
@@ -0,0 +1,76 @@
+package org.firstinspires.ftc.teamcode;
+
+//template for autonomous when needed
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@TeleOp(name = "Robot2AutonomousV1 (Blocks to Java)")
+public class Robot2AutonomousV1 extends LinearOpMode {
+
+  private ElapsedTime     runtime = new ElapsedTime();
+
+  private DcMotor rightRearMotor;
+  private DcMotor rightFrontMotor;
+  private DcMotor leftRearMotor;
+  private DcMotor leftFrontMotor;
+  private DcMotor strafeMotor;
+
+  private CRServo rightIntake;
+  private CRServo leftIntake;
+
+  //power to left/right side of drivetrain
+  double leftMotorPower;
+  double rightMotorPower;
+
+
+  @Override
+  public void runOpMode() {
+    //hardware configuration, naming all motors/servos and configuring direction/behaviour
+
+    //motor config
+    rightRearMotor = hardwareMap.get(DcMotor.class, "right_rear_motor");
+    rightFrontMotor = hardwareMap.get(DcMotor.class, "right_front_motor");
+    leftRearMotor = hardwareMap.get(DcMotor.class, "left_rear_motor");
+    leftFrontMotor = hardwareMap.get(DcMotor.class, "left_front_motor");
+    strafeMotor = hardwareMap.get(DcMotor.class, "strafe_motor");
+
+    //servo config
+    rightIntake = hardwareMap.get(CRServo.class, "right_intake");
+    leftIntake = hardwareMap.get(CRServo.class, "left_intake");
+
+    //direction fixing (so all motors drive in the same direction)
+    leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    rightIntake.setDirection(DcMotorSimple.Direction.REVERSE);
+
+    //prevent robot from rolling when power is cut
+    rightRearMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    rightFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftRearMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+    leftFrontMotor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+    //opMode loop, this is what actually runs when you press start.
+    waitForStart();
+    if (opModeIsActive()) {
+      while (opModeIsActive()) {
+        //main drive loop, methods here are called repeatedly while active
+
+
+
+      }
+    }
+  }
+
+
+  private void Telemetry() {
+
+    telemetry.addData("Left", leftMotorPower);
+    telemetry.addData("Right", rightMotorPower);
+    telemetry.update();
+  }
+}
